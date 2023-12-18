c53e6cc9ec342682b362907cfbea588ea860fe86
ahrisai <sasaslapik40@gmail.com>
2023-12-18 13:31:02 +0300

    not finished startPage

---
+++ src/pages/StartPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+const StartPage = () => {
+  return (
+
+    <>
+        <Header/>
+        <main></main>
+        <Footer/>
+    </>
+  )
+}
+
+export default StartPage
