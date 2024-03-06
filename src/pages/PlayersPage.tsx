fc396a66a293b9003529185ac3f9d8ddb86e0deb
ahrisai <sasaslapik40@gmail.com>
2024-03-06 09:25:26 +0300

    fix player avatar

--- src/pages/PlayersPage.tsx
+++ src/pages/PlayersPage.tsx
@@ -1,16 +1,53 @@
+import Container from '../components/Container';
 import styled from 'styled-components';
 
 const PlayersPage = () => {
   return (
-    <Chota>
-      {' '}
-      <div>PlayersPage</div>
-    </Chota>
+    <Main>
+      <Container>
+        <MainContainer>
+          <LeftContainer>
+            <SearchBar></SearchBar>
+            <ListContainer></ListContainer>
+          </LeftContainer>
+          <RightContainer>
+            <FilterBar></FilterBar>
+          </RightContainer>
+        </MainContainer>
+      </Container>
+    </Main>
   );
 };
 
-const Chota = styled.div`
-  height: 100vh;
+const Main = styled.main`
+  padding-block: 20px;
 `;
+
+const MainContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+  height: 80vh;
+`;
+
+const LeftContainer = styled.div`
+  width: 70%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+`;
+const RightContainer = styled.div`
+  width: 28%;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const SearchBar = styled.div``;
+const ListContainer = styled.div``;
+
+const FilterBar = styled.div``;
 
 export default PlayersPage;
